0443286be9ee5c25c10eeaee9b754ceab02ddd5c
harbinzhang <seashore.real@gmail.com>
2018-09-23 10:26:38 -0700

    auto

--- main.py
+++ main.py
@@ -14,31 +14,22 @@ import logging
 import boto3
 from botocore.exceptions import ClientError
 
+DEV_MODE = True if os.environ.get("DEV_MODE", "") else False
+
+ENV = os.environ["ENV"]
+APP_SPEC = os.environ["APP_SPEC"]
+BOTO_REGION = os.environ["BOTO_REGION"]
+
+if BOTO_REGION == "":
+    BOTO_REGION = os.environ["AWS_DEFAULT_REGION"]
+
+ENI_TAG_KEY = "exhibitor-eni-pool"
+ENI_TAG_VALUE = "{}-exhibitor-{}-eni-pool".format(ENV, APP_SPEC).replace("exhibitor--eni", "exhibitor-eni")
+
+
 
 @functools.lru_cache(1)
 def get_metadata():
-    """
-    Get ec2 instance metadata for the machine running this code.
-    calls:
-        http://169.254.169.254/latest/dynamic/instance-identity/document
-    example answer:
-    {
-      "devpayProductCodes" : null,
-      "privateIp" : "10.0.0.1",
-      "availabilityZone" : "eu-central-1b",
-      "version" : "2010-08-31",
-      "region" : "eu-central-1",
-      "instanceId" : "i-xxxxxxxxxxxxxxx",
-      "billingProducts" : null,
-      "instanceType" : "t2.micro",
-      "pendingTime" : "2017-03-07T11:18:20Z",
-      "accountId" : "xxxxxxxxxxxx",
-      "architecture" : "x86_64",
-      "kernelId" : null,
-      "ramdiskId" : null,
-      "imageId" : "ami-xxxxxxxxx"
-    }
-    """
     if not DEV_MODE:
         conn = http.client.HTTPConnection("169.254.169.254", 80, timeout=10)
         conn.request("GET", "/latest/dynamic/instance-identity/document")
