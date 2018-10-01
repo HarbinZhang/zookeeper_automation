07b61738d959046a5ad985fa12f947aec09245e3
harbinzhang <seashore.real@gmail.com>
2018-10-01 13:29:58 -0700

    auto

--- eip/test.py
+++ eip/test.py
@@ -1,11 +1,12 @@
 import boto3
+import os
 
 BOTO_REGION = os.environ.get("BOTO_REGION", "")
 
 if BOTO_REGION == "":
     BOTO_REGION = os.environ.get("AWS_DEFAULT_REGION", "us-east-2")
 
-    
+
 ec2 = boto3.client('ec2', region_name=BOTO_REGION)
 filters = [
     {'Name': 'domain', 'Values': ['vpc']}
